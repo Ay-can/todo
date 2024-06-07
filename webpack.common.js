c1a0601e9bbab661fe76069dd1fd0e1290e32ea1
Aycan <aycan.acn2020@gmail.com>
2024-06-07 16:05:14 +0200

    allow images as webpack resource

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
